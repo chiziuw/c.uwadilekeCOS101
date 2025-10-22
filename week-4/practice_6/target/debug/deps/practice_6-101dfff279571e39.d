65171c833646228ab91023c813a1f8c86277bc7a
Chizaram <chizaramnathan@gmail.com>
2025-10-22 16:46:18 +0100

    (For loop illustration)

---
+++ week-4/practice_6/target/debug/deps/practice_6-101dfff279571e39.d
@@ -0,0 +1,5 @@
+C:\Users\chiza\c.uwadilekeCOS101\week-4\practice_6\target\debug\deps\practice_6-101dfff279571e39.d: src\main.rs
+
+C:\Users\chiza\c.uwadilekeCOS101\week-4\practice_6\target\debug\deps\practice_6-101dfff279571e39.exe: src\main.rs
+
+src\main.rs:
---
+++ week-4/practice_6/target/debug/practice_6.d
@@ -0,0 +1,1 @@
+C:\Users\chiza\c.uwadilekeCOS101\week-4\practice_6\target\debug\practice_6.exe: C:\Users\chiza\c.uwadilekeCOS101\week-4\practice_6\src\main.rs
