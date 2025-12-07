9abe0a0f69b964d1c3f27d493d91e5517cf7c2d8
Chizaram <chizaramnathan@gmail.com>
2025-12-07 23:31:25 +0100

    Week 9 Project

--- week-10/project-1/src/main.rs
+++ week-10/project-1/src/main.rs
@@ -1,3 +1,24 @@
+struct LaptopPrice{
+    hp_laptops:u32,
+    ibm_laptops:u32,
+    toshiba_laptops:u32,
+    dell_laptops:u32
+}
+
+impl LaptopPrice{
+    fn total_cost(&self)->u32{
+        3*self.hp_laptops + 3*self.ibm_laptops + 3*self.toshiba_laptops + 3*self.dell_laptops
+    }
+}
+
+
+
 fn main() {
-    println!("Hello, world!");
+    let shop = LaptopPrice{
+        hp_laptops:650000,
+        ibm_laptops:755000,
+        toshiba_laptops:550000,
+        dell_laptops: 850000
+    };
+    println!("The total cost of buying 3 of each brand is: N{}", shop.total_cost());
 }
