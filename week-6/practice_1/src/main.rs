3e7cc6e03a74707eb41303a0ca03e5ca7fbd73eb
Chizaram <chizaramnathan@gmail.com>
2026-01-21 16:54:01 +0100

    Rust Strings & Operators

---
+++ week-6/practice_1/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+  let name = "Aisha Lawal";
+  let uni:&str = "Pan-Atlantic University";
+  let addr:&str = "km 52 Lekki Expressway, Ibeju-Lekki, Lagos";
+  println!("Name: {}",name );
+  println!("UNiversity: {} \n Address: {}",uni, addr );
+
+  let department:& str = "computer Science";
+  let school:& str = "schoo of science of technology";
+  println!("Depatment: {}, \nSchool: {}",department, school );
+}
