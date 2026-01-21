121f16d519a0d211df5fc3b91f3e784e064efef4
Chizaram <chizaramnathan@gmail.com>
2026-01-21 11:46:07 +0100

    Rust Cargo & Data Types

---
+++ week-3/practice_1/target/debug/deps/pracyice_1.d
@@ -0,0 +1,5 @@
+C:\Users\tolud\Documents\t.da-silvaCOS101\week-3\practice_1\target\debug\deps\pracyice_1.d: src\main.rs
+
+C:\Users\tolud\Documents\t.da-silvaCOS101\week-3\practice_1\target\debug\deps\pracyice_1.exe: src\main.rs
+
+src\main.rs:
