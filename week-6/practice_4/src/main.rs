8d817275a679aa4028ec708a71edc2e27ffe3b9c
Chizaram <chizaramnathan@gmail.com>
2026-01-21 16:50:23 +0100

    Illustration: push_str()

---
+++ week-6/practice_4/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+   let fullname = "Chidubem John Umeh";
+   let department = "Computer Science";
+   let uni = "Pan-Atlantic University";
+
+
+   let mut school = "School of Science".to_string();
+   school.push_str(" and tschnology");
+
+    println!("My name is: {}", fullname);
+    println!("The length my fullname is: {}",fullname.len());
+    println!("I am a student of {} Department", department );
+    println!("{}",school);
+    println!("{}",uni);
+}
