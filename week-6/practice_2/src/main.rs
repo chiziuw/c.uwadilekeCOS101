1b277b6522f029a9fe7ce97bc2ee4d6264942630
Chizaram <chizaramnathan@gmail.com>
2026-01-21 16:49:21 +0100

    String Object Syntax

---
+++ week-6/practice_2/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+   let empty_string = String::new();
+    println!("Length of empty_string is {}", empty_string);
+   let content_string = String::from("Computer science");
+   print!("Length of content_string is {}", content_string);
+}
