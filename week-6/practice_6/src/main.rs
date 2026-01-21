a9c0ffdc95f830eaa525da92a7dbff11f55e5e64
Chizaram <chizaramnathan@gmail.com>
2026-01-21 16:51:08 +0100

    Illustration: String Concatenation

---
+++ week-6/practice_6/src/main.rs
@@ -0,0 +1,20 @@
+fn main() {
+   
+   let n1 = "Electrical".to_string();
+   let n2 = "Electronic".to_string();
+   let n3 = "Engineering".to_string();
+   let n4 = n1 + &n2 + &n3;
+
+
+    println!("\nThe {} is informed by the aspiration 
+    to train electrical/ electronic engineering professionls 
+    in the areas of design,  building and maintenance of electrical control system,",n4);
+    let v1 = "Computer".to_string();
+    let v2 = "Science".to_string();
+    let v3 = v1 + &v2; 
+
+    println!();
+    println!("{} is aimed at developing competent,  creative
+        innovative, entreprenaural and ethicalluy-minded persons,
+        Computer Science.",v3);
+}
